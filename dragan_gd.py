d69026f2b730e8c973d5bdaa7a6bb30910bd615d
chenzhihan2020 <39210258+chenzhihan2020@users.noreply.github.com>
2019-06-10 17:52:35 -0700

    Update dragan_gd.py

--- dragan_gd.py
+++ dragan_gd.py
@@ -16,28 +16,12 @@ import torch.nn as nn
 #import torch.nn.functional as F
 import torch
 
-os.makedirs("images", exist_ok=True)
-os.makedirs("models", exist_ok=True)
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
-parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
-parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
-parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
-parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
-parser.add_argument("--channels", type=int, default=3, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
-opt = parser.parse_args()
-print(opt)
-
-#cuda = True if torch.cuda.is_available() else False
-device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+latent_dim=100
+channels=3
+img_size=32
 
 
-def weights_init_normal(m):
+def weights_init(m):
     classname = m.__class__.__name__
     
     if classname.find("OctaveConv") != -1:
@@ -189,8 +173,8 @@ class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
 
-        self.init_size = opt.img_size // 4
-        self.l1 = nn.Sequential(nn.Linear(opt.latent_dim, 128 * self.init_size ** 2))
+        self.init_size = img_size // 4
+        self.l1 = nn.Sequential(nn.Linear(latent_dim, 128 * self.init_size ** 2))
 
         self.conv_blocks = nn.Sequential(
             nn.BatchNorm2d(128),
@@ -202,7 +186,7 @@ class Generator(nn.Module):
             OctaveConv(128, 64, 3, stride=1, padding=1),
             dual_channel_batchnorm2d(64, 0.8),
             dual_channel_leakyrelu(0.2, inplace=True),
-            OctaveConv(64, opt.channels, 3, stride=1, padding=1, alpha_out = 0),
+            OctaveConv(64, channels, 3, stride=1, padding=1, alpha_out = 0),
             high_channel_tanh(),
         )
 
@@ -226,14 +210,14 @@ class Discriminator(nn.Module):
             return block
 
         self.model = nn.Sequential(
-            *discriminator_block(opt.channels, 16, alpha_in=0, bn=False),
+            *discriminator_block(channels, 16, alpha_in=0, bn=False),
             *discriminator_block(16, 32),
             *discriminator_block(32, 64),
             *discriminator_block(64, 128, alpha_out=0),
         )
 
         # The height and width of downsampled image
-        ds_size = opt.img_size // 2 ** 4
+        ds_size = img_size // 2 ** 4
         self.adv_layer = nn.Sequential(nn.Linear(128 * ds_size ** 2, 1), nn.Sigmoid())
 
     def forward(self, img):
@@ -251,36 +235,6 @@ adversarial_loss = torch.nn.BCELoss()
 # Loss weight for gradient penalty
 lambda_gp = 10
 
-# Initialize generator and discriminator
-generator = Generator()
-discriminator = Discriminator()
-
-generator.to(device)
-discriminator.to(device)
-adversarial_loss.to(device)
-
-# Initialize weights
-generator.apply(weights_init_normal)
-discriminator.apply(weights_init_normal)
-
-# Configure data loader
-os.makedirs("data/cifar10", exist_ok=True)
-dataloader = torch.utils.data.DataLoader(
-    datasets.CIFAR10(
-        "images/cifar10",
-        train=True,
-        download=True,
-        transform=transforms.Compose(
-            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
-        ),
-    ),
-    batch_size=opt.batch_size,
-    shuffle=True,
-)
-
-# Optimizers
-optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
 
 Tensor = torch.FloatTensor
 
@@ -310,66 +264,3 @@ def compute_gradient_penalty(D, X):
     gradient_penalty = lambda_gp * ((gradients.norm(2, dim=1) - 1) ** 2).mean()
     return gradient_penalty
 
-
-# ----------
-#  Training
-# ----------
-
-for epoch in range(opt.n_epochs):
-    for i, (imgs, _) in enumerate(dataloader):
-        t = time.time()
-
-        # Adversarial ground truths
-        valid = Tensor(imgs.shape[0], 1).fill_(1.0).to(device)
-        fake = Tensor(imgs.shape[0], 1).fill_(0.0).to(device)
-
-        # Configure input
-        real_imgs = imgs.type(Tensor).to(device)
-
-        # -----------------
-        #  Train Generator
-        # -----------------
-
-        optimizer_G.zero_grad()
-
-        # Sample noise as generator input
-        z = Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))).to(device)
-
-        # Generate a batch of images
-        gen_imgs = generator(z)
-
-        # Loss measures generator's ability to fool the discriminator
-        g_loss = adversarial_loss(discriminator(gen_imgs), valid)
-
-        g_loss.backward()
-        optimizer_G.step()
-
-        # ---------------------
-        #  Train Discriminator
-        # ---------------------
-
-        optimizer_D.zero_grad()
-
-        # Measure discriminator's ability to classify real from generated samples
-        real_loss = adversarial_loss(discriminator(real_imgs), valid)
-        fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
-        d_loss = (real_loss + fake_loss) / 2
-
-        # Calculate gradient penalty
-        gradient_penalty = compute_gradient_penalty(discriminator, real_imgs.data)
-        gradient_penalty.backward()
-
-        optimizer_D.step()
-
-        time_elapse = time.time() - t;
-        print(
-            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f] [time: %f ]"
-            % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item(), time_elapse)
-        )
-
-    #save_image(gen_imgs.data, "images/%d.png" % epoch, nrow=int(math.sqrt(opt.batch_size)), normalize=True)
-        batches_done = epoch * len(dataloader) + i
-        if batches_done % opt.sample_interval == 0:
-            save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
-            torch.save(generator.state_dict(), 'models/model_G_%d.pth' % batches_done)
-            torch.save(discriminator.state_dict(), 'models/model_D_%d.pth' % batches_done)
