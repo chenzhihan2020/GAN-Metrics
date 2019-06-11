ae901d57ea210099ce439567a132ec0c586ae5af
chenzhihan2020 <39210258+chenzhihan2020@users.noreply.github.com>
2019-06-10 17:49:41 -0700

    Update gan_eval.py

--- gan_eval.py
+++ gan_eval.py
@@ -147,7 +147,7 @@ if __name__ == '__main__':
 
             output = netD(real_cpu)
             errD_real = criterion(output, label)
-            errD_real.backward()
+
             D_x = output.mean().item()
 
             # train with fake
@@ -157,9 +157,13 @@ if __name__ == '__main__':
             label.fill_(fake_label)
             output = netD(fake.detach())
             errD_fake = criterion(output, label)
-            errD_fake.backward()
+
             D_G_z1 = output.mean().item()
-            errD = errD_real + errD_fake
+            if(opt.inputmodel=='dragan' or opt.inputmodel=='dragan_gd'):
+                errD = inputmodel.compute_gradient_penalty(netD, real_cpu.data)
+            else:
+                errD = (errD_real + errD_fake) / 2
+            errD.backward()
             optimizerD.step()
 
             ############################
@@ -168,7 +172,7 @@ if __name__ == '__main__':
             netG.zero_grad()
             label.fill_(real_label)  # fake labels are real for generator cost
             output = netD(fake)
-            errG = criterion(output, label)
+            errG = criterion(output, label)           
             errG.backward()
             D_G_z2 = output.mean().item()
             optimizerG.step()
